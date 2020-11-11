26b11ddc3aa51100aa5dbaf8e049d9819301db16
Danyi Lin <1489044010@qq.com>
2020-11-11 15:45:06 +0800

    Submission for Module 1

---
+++ DanyiLin/Module1/DataConversion.py
@@ -0,0 +1,4 @@
+price =10
+print("How many beers you want?")
+print("Your total price is: $" +price * input())
+#print("Your total price is: $" +str(price * input()))
---
+++ DanyiLin/Module1/ForControl.py
@@ -0,0 +1,12 @@
+import random
+
+for i in range(1, random.randint(5,15)):
+    print("This for loop has already run " + str(i) + "times.")
+
+
+fruits =['banana','apple','mango']
+for index in range(len(fruits)):
+    print('Current fruit:',fruits[index])
+
+    print('Good bye!')
+    
