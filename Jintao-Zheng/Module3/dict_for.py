d9dac8fe8bf333f22f51e1fada3c737e2308c89d
Jintao Zheng <2399347187@qq.com>
2020-11-12 22:05:39 +0800

    Submission for Module 3

---
+++ Jintao-Zheng/Module3/dict_for.py
@@ -0,0 +1,8 @@
+alien_0 = {'color': 'green', 'points': 5}
+alien_1 = {'color': 'yellow', 'points': 10}
+
+aliens = [alien_0,alien_1]
+
+for i in aliens:
+    for key,value in i.items():
+        print("KEY: ", key, "\t", "VALUE :", value)
