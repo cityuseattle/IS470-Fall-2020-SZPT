831604c72bf147cc09aefb8db069902c3b6d0968
Yuting Li(Gween)830127826 <2292369319@qq.com>
2020-11-12 09:47:47 +0800

    Submission for Module03

--- Yuting-Li/Module03/dict_for.py
+++
@@ -1,8 +0,0 @@
-alien_0 = {'color': 'green', 'points':5}
-alien_1 = {'color': 'yellow', 'points':10}
-
-aliens = [alien_0,alien_1]
-
-for i in aliens:
-    for key,value in i.items():
-        print("KEY: ", key, "\t" , "VALUE :", value)
