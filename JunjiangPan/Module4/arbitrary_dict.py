146ce64a9d53315cdccba1b0256b7fcaab83d043
JunjiangPan <1742004648@qq.com>
2020-11-13 23:21:08 +0800

    Submission for Module 3

---
+++ JunjiangPan/Module4/arbitrary_dict.py
@@ -0,0 +1,7 @@
+def menu(item, quan, **restaurant):
+    restaurant['Item'] = item
+    restaurant['Quantity'] = quan
+    return restaurant
+
+restaurant = menu('soup', '1',LOcation='seattle',Zipcode='98109')
+print(restaurant)
