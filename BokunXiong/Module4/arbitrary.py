58cb3cd3fecc65d5fa83040417105edcad7d834f
BokunXiong <515525612@qq.com>
2020-11-13 16:19:44 +0800

    Submission for Module 4

---
+++ BokunXiong/Module4/arbitrary.py
@@ -0,0 +1,6 @@
+def menu(*foods):
+    print(foods)
+
+
+menu("pizza")
+menu("stop", "fries","pizza","pasta")
---
+++ BokunXiong/Module4/positional.py
@@ -0,0 +1,5 @@
+def describle_pet(animal_type, pet_name):
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title}.")
+describle_pet('hamster', 'harry')
+describle_pet('dog', 'while')
