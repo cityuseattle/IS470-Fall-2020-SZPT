f59c6919836ac50f2d12d442178ae9c1776d0b59
Danyi Lin <1489044010@qq.com>
2020-11-18 17:45:12 +0800

    Submission for Module 9

---
+++ DanyiLin/Module09/01-ttd-example.py
@@ -0,0 +1,26 @@
+import unittest 
+
+class Solution:
+    def plus_seven_to_positive_num(self, x):
+        if x > 100 or x < 0:
+            return -1
+        return x + 7
+
+
+a = Solution
+class Test(unittest.TestCase):
+
+    def test_positive(self):
+        self.assertEqual(a.plus_seven_to_positive_num(self,100),107)
+
+
+    def test_nagative_num(self):
+        self.assertEqual(a.plus_seven_to_positive_num(self,-100),-1)
+
+    
+    def test_more_than_100(self):
+        self.assertEqual(a.plus_seven_to_positive_num(self,101),-1)
+
+
+if __name__ == '__main__':
+    unittest.main()
