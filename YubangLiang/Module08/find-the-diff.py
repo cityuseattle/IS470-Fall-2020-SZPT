b533925855da7733f6e4f5ad72c94774b9fcff32
YubangLiang <709239031@qq.com>
2020-11-18 21:28:06 +0800

    Submission for Module 8

---
+++ YubangLiang/Module08/find-the-diff.py
@@ -0,0 +1,25 @@
+import unittest
+
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        s_list = sorted(s)
+        t_list = sorted(t)
+        for i in range(len(s_list)):
+            if s_list[i] != t_list[i]:
+                return s_list[i]
+        return t_list[-1]
+
+a = Solution
+class Test(unittest.TestCase):
+
+    def test_diff_at_end(self):
+        self.assertEqual(a.findTheDifference(self,"abcd", "abcde"), "e")
+    def test_diff_at_begin(self):
+        self.assertEqual(a.findTheDifference(self,"abc", "abcd"), "d")
+    def test_diff_at_2nd_to_last(self):
+        self.assertEqual(a.findTheDifference(self,"bcd", "bcde"), "e")
+    def test_empty(self):
+        self.assertEqual(a.findTheDifference(self,"", "a"), "a")
+    
+if __name__ == '__main__':
+    unittest.main()
