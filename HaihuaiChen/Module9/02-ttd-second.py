6cbf59e0b633db132cd2db24ec3a99595dbafe3f
ChenHaihuai <haihuai@outlook.com>
2020-11-19 22:19:53 +0800

    Submission for Module 9

---
+++ HaihuaiChen/Module9/02-ttd-second.py
@@ -0,0 +1,21 @@
+import unittest
+
+class Solution:
+    def product_of_list(self, nums):
+        product = 1
+        for i in range(len(nums)):
+            product *= nums[i]
+        return product
+
+a = Solution
+
+class Test(unittest.TestCase):
+
+    def test_product_of_elements(self):
+        self.assertEqual(a.product_of_list(self, [1,3,5]), 15)
+
+    def test_with_zero_element(self):
+        self.assertEqual(a.product_of_list(self, [1,3,0]), 0)
+
+if __name__ == '__main__':
+    unittest.main()
