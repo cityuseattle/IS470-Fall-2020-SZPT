3db3a2a20089e1e9a5344c32e3d6b0794fbaecab
kyogre1492 <1546275200@qq.com>
2020-11-20 16:11:43 +0800

    Submission for Module10（web）

---
+++ jiaming-zhang/Module10（web）/automate_web.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+browser.get("https://accounts.google.com/signup")
+firstname = browser.find_element_by_id("firstName")
+firstname.send_keys("Thomas")
+lastname = browser.find_element_by_id("lastName")
+lastname.send_keys("Anderson")
+username = browser.find_element_by_name("username")
+username.send_keys("Neo")
+passwd = browser.find_element_by_name("Passwd")
+passwd.send_keys("noPassword")
+
+passwd=browser.find_element_by_name("ComfirmPasswd")
+passwd.send_keys("noPassword")
